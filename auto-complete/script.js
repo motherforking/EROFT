4c7cb22c551032456d21ff86476f56da8e6b5849
motherforking <63572385+motherforking@users.noreply.github.com>
2023-10-31 17:23:45 -0400

    format js

--- auto-complete/script.js
+++ auto-complete/script.js
@@ -1,16 +1,16 @@
 var box = document.getElementById("channelButton");
-        var spirit = document.querySelector('input[name="spirit"]:checked').value;
-        box.onclick = function() {channel()};
-        function channel(){
-            switch(spirit){
-                case "lincoln":
-                    box.placeholer="four score and seven years ago";
-                    break;
-                case "elvis":
-                    box.placeholer="hey pretty mama";
-                    break;
-                case "swift":
-                    box.placeholer="i shake it off, i shake it off";
-                    break;
+var spirit = document.querySelector('input[name="spirit"]:checked').value;
+box.onclick = function() {channel()};
+function channel(){
+    switch(spirit){
+        case "lincoln":
+            box.placeholer="four score and seven years ago";
+            break;
+        case "elvis":
+            box.placeholer="hey pretty mama";
+            break;
+        case "swift":
+            box.placeholer="i shake it off, i shake it off";
+            break;
             }
         }
